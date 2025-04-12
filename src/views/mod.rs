8cc80cf1e0b6ce886cadf56a3b358a1aabc233d6
valentino grande <valentinogrande972@gmail.com>
2025-04-12 11:23:43 -0300

    reneme a file

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,7 +1,7 @@
 pub mod login;
 pub mod register;
 pub mod logout;
-pub mod create_assesment;
+pub mod create_assessment;
 pub mod create_submission;
 pub mod register_testing_users;
 pub mod upload_profile_picture;
