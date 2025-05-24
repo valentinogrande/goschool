67eab7f84a86a648de47f9d05deb1088f90e804b
valentino grande <valentinogrande972@gmail.com>
2025-05-24 13:07:55 -0300

    get grades was moved to oop

--- src/views/get_grades.rs
+++ src/views/get_grades.rs
@@ -23,11 +23,6 @@ pub async fn get_grades(
     req: HttpRequest,
     filter: web::Query<GradeFilter>,
 ) -> impl Responder {
-
-    // importante:
-    // esto te deberia dar todas las grades disponebles por usuario, despues se 
-    // deberia poder filtar por el usuario que interesa.
-
     let cookie = match req.cookie("jwt") {
         Some(cookie) => cookie,
         None => return HttpResponse::Unauthorized().json("Missing JWT cookie"),
@@ -40,43 +35,9 @@ pub async fn get_grades(
     
     let user = token.claims.user;  
 
-    let student_id = match filter.student_id {
-        Some(id) => id,
-        None => return HttpResponse::BadRequest().json("Missing student_id"),
-    };
-
-    if user.role != Role::teacher{}
-    let students_id = match user.get_students(pool.clone(), None).await
-        {
-            Ok(r) => r,
-            Err(_) => return HttpResponse::InternalServerError().finish(),
-        };
-    
-    if !students_id.contains(&student_id) {
-            return HttpResponse::Unauthorized().json("Not authorized to access this student's data");
-    }
-
-    let mut query = QueryBuilder::new("SELECT * FROM grades WHERE student_id = ");
-    query.push_bind(student_id);
-
-    if let Some(subject_id) = filter.subject_id {
-        query.push(" AND subject_id = ");
-        query.push_bind(subject_id);
-    }
-
-    if let Some(ref description) = filter.description {
-        query.push(" AND description LIKE ");
-        query.push_bind(format!("%{}%", description));
-    }
-    
-
-    let grades: Vec<Grade> = match query
-        .build_query_as()
-        .fetch_all(pool.get_ref())
-        .await
-    {
-        Ok(r) => r,
-        Err(e) => return HttpResponse::InternalServerError().body(e.to_string()),
+    let grades = match user.get_grades(pool, Some(filter.into_inner())).await { 
+        Ok(g) => g,
+        Err(e) => return HttpResponse::InternalServerError().json(e.to_string()),
     };
 
     HttpResponse::Ok().json(grades)
