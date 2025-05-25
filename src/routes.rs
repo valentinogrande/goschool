7d8c3b9b55aca7df763b5b0c7e06811df2d2a7e2
valentino grande <valentinogrande972@gmail.com>
2025-05-25 18:31:44 -0300

    routes.rs updated

--- src/routes.rs
+++ src/routes.rs
@@ -1,43 +1,42 @@
 use actix_web::web;
 
 use crate::views::{
-    courses::get_courses,
-    subjects::get_subjects,
-    submissions::post_homework_submission,
     assessmets::{get_assessments, post_assessment},
+    courses::get_courses,
     grades::{get_grades, post_grade},
-    personal_data::get_personal_data,
-    profile_pictures::{get_profile_picture, post_profile_picture},
-    role::get_role,
-    roles::get_roles,
-    messages::{get_messages, post_message},
     login::login,
     logout::logout,
+    messages::{get_messages, post_message},
+    personal_data::get_personal_data,
+    profile_pictures::{get_profile_picture, post_profile_picture},
     register::{register, register_testing_users},
+    role::get_role,
+    roles::get_roles,
+    subjects::get_subjects,
+    submissions::post_homework_submission,
     verify_token::verify_token,
     //create_submission_selfassable::create_selfassessable_submission,
 };
 
-
 pub fn register_services(cfg: &mut web::ServiceConfig) {
-    cfg.service(register)
-        .service(verify_token)
-        .service(login)
-        .service(logout)
-        .service(post_homework_submission)
-        .service(get_assessments)
+    cfg.service(get_assessments)
+        .service(get_courses)
         .service(get_grades)
+        .service(get_messages)
         .service(get_personal_data)
-        .service(post_assessment)
-        .service(post_profile_picture)
         .service(get_profile_picture)
         .service(get_role)
         .service(get_roles)
-        .service(post_message)
-        .service(get_messages)
-        .service(post_grade)
-        //.service(create_selfassessable_submission)
         .service(get_subjects)
-        .service(get_courses)
-        .service(register_testing_users);
+        .service(login)
+        .service(logout)
+        .service(post_assessment)
+        .service(post_grade)
+        .service(post_homework_submission)
+        .service(post_message)
+        .service(post_profile_picture)
+        .service(register)
+        .service(register_testing_users)
+        .service(verify_token);
+        //.service(create_selfassessable_submission)
 }
